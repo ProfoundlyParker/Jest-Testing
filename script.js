ddee65d56c28c98f456252e318891fda0d3dc597
ProfoundlyParker <parkerramey@yahoo.com>
2023-09-26 16:49:05 -0400

    Adding files - testing exercise with Jest

---
+++ script.js
@@ -0,0 +1,20 @@
+const googleWebsites = [
+    'cats.com',
+    'souprecipes.com',
+    'animals.com',
+    'flowers.com',
+    'catpictures.com',
+    'myfavouritecats.com',
+    'myfavouritecats2.com'
+];
+
+const googleSearch = (searchInput, db) => {
+    const matches = db.filter(website => {
+        return website.includes(searchInput);
+    })
+    return matches.length > 3 ? matches.slice(0, 3) : matches;
+}
+
+// console.log(googleSearch('cat', googleWebsites));
+
+module.exports = googleSearch;
